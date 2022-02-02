648c4d3652dd1a99db481c92c6b4545471e871de
Debashis <debu123prokis@gmail.com>
2022-02-02 07:49:12 +0530

    adding back button

--- jsonMod/index.js
+++ jsonMod/index.js
@@ -5,7 +5,7 @@ const { resetWatchers } = require('nodemon/lib/monitor/watch');
 const server=http.createServer((req,res)=>{
     if(req.url=="/"){
         res.end("Hello");
-    }else if(req.url=="/jsonapi"){
+    }else if(req.url=="/jsonapideb"){
         fs.readFile("../jsonMod/chap.json","UTF-8",(error,data)=>{
            console.log(data);
            res.end(data);
